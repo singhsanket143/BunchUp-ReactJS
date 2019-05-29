62b70bc6f0b002d49b2a465e6aa902ca1e1e8c52
singhsanket143 <singhsanket143@gmail.com>
2019-05-29 22:41:01 +0530

    Updated Readme

--- src/features/event/EventDashboard/EventDashboard.js
+++ src/features/event/EventDashboard/EventDashboard.js
@@ -6,49 +6,49 @@ import EventForm from "../EventForm/EventForm";
 const eventsDashboard = [
   {
     id: "1",
-    title: "Trip to Tower of London",
+    title: "Machine Learning Workshop At Coding Blocks",
     date: "2018-03-27T11:00:00+00:00",
-    category: "culture",
+    category: "Technology",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-    city: "London, UK",
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: "Bob",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+    city: "Delhi, India",
+    venue: "Coding Blocks, Dwarka",
+    hostedBy: "Sanket Singh",
+    hostPhotoURL: "https://randomuser.me/api/portraits/men/24.jpg",
     attendees: [
       {
         id: "a",
-        name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        name: "Sarthak",
+        photoURL: "https://randomuser.me/api/portraits/men/25.jpg"
       },
       {
         id: "b",
-        name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        name: "Rishab",
+        photoURL: "https://randomuser.me/api/portraits/men/26.jpg"
       }
     ]
   },
   {
     id: "2",
-    title: "Trip to Punch and Judy Pub",
+    title: "GSoC Meetup At Coding Blocks",
     date: "2018-03-28T14:00:00+00:00",
-    category: "drinks",
+    category: "Technology and Culture",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-    city: "London, UK",
-    venue: "Punch & Judy, Henrietta Street, London, UK",
-    hostedBy: "Tom",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
+    city: "Noida, India",
+    venue: "Coding Blocks Noida, Uttar Pradesh, India",
+    hostedBy: "Rishabh Bansal",
+    hostPhotoURL: "https://randomuser.me/api/portraits/women/22.jpg",
     attendees: [
       {
         id: "b",
         name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        photoURL: "https://randomuser.me/api/portraits/men/69.jpg"
       },
       {
         id: "a",
         name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        photoURL: "https://randomuser.me/api/portraits/men/54.jpg"
       }
     ]
   }
@@ -66,7 +66,8 @@ class EventDashboard extends React.Component {
     // this.handleFormOpen = this.handleFormOpen.bind(this); Use this when not using arrow function
   }
 
-  handleFormOpen = () => {
+  handleFormOpen = msg => {
+    console.log(msg);
     this.setState({ isOpen: true });
   };
 
@@ -82,7 +83,7 @@ class EventDashboard extends React.Component {
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
-            onClick={this.handleFormOpen}
+            onClick={() => this.handleFormOpen("Clicked")}
             positive
             content="Create Event"
           />
