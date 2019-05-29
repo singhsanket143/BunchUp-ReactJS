56fc82a4a67fa8bbdb2dd5004061989dd7f260fc
singhsanket143 <singhsanket143@gmail.com>
2019-05-29 18:49:28 +0530

    Added Hot Reload Using Webpacks module.hot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+// Adding Hot Reload using Webpacks module.hot
+ReactDOM.render(<App />, rootEl);
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
