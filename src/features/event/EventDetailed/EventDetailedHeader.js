a239b500acdde75336d959e42bd5e4dd891b6437
singhsanket143 <singhsanket143@gmail.com>
2019-06-01 00:30:54 +0530

    Revamped header

--- src/features/event/EventDetailed/EventDetailedHeader.js
+++ src/features/event/EventDetailed/EventDetailedHeader.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { Segment, Image, Button, Item, Header } from "semantic-ui-react";
+
+const eventImageStyle = {
+  filter: "brightness(30%)"
+};
+
+const eventImageTextStyle = {
+  position: "absolute",
+  bottom: "5%",
+  left: "5%",
+  width: "100%",
+  height: "auto",
+  color: "white"
+};
 
 const EventDetailedHeader = () => {
-  return <div>Header</div>;
+  return (
+    <Segment.Group>
+      <Segment basic attached="top" style={{ padding: "0" }}>
+        <Image
+          src="/assets/categoryImages/drinks.jpg"
+          fluid
+          style={eventImageStyle}
+        />
+
+        <Segment basic style={eventImageTextStyle}>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content="Event Title"
+                  style={{ color: "white" }}
+                />
+                <p>Event Date</p>
+                <p>
+                  Hosted by <strong>Hosted by</strong>
+                </p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      </Segment>
+
+      <Segment attached="bottom">
+        <Button>Cancel My Place</Button>
+        <Button color="teal">JOIN THIS EVENT</Button>
+
+        <Button color="orange" floated="right">
+          Manage Event
+        </Button>
+      </Segment>
+    </Segment.Group>
+  );
 };
 
 export default EventDetailedHeader;
